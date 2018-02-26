4e4913e9262e8af30205bea3fe163e591c2c12a4
Matthew Thomas Kelly <MatthewThomasKelly@gmail.com>
2018-02-26 21:46:17 +0000

    Tidy up of messed up structure

--- WebUAuctionSearch/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class WebuauctionsearchConfig(AppConfig):
-    name = 'WebUAuctionSearch'
--- WebUtilities/README.md
+++
@@ -1,2 +0,0 @@
-# WebUtilities
-Python django powered website to query and carve auction sites
